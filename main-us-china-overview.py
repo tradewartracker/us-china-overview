b33e097e00e5f02c831817e81ee7a1a472e3b7fc
mikew <waugh.e.michael@gmail.com>
2021-03-25 12:59:50 -0400

    updated data febuary

--- main-us-china-overview.py
+++ main-us-china-overview.py
@@ -24,7 +24,7 @@ height = int(1.15*533)
 width = int(1.15*750)
 
 start_date = "2013-01-01"
-end_date = "2021-01-01"
+end_date = "2021-02-01"
 
 crl = ["darkblue","slategray","crimson"]
 
@@ -36,7 +36,7 @@ df = pq.read_table(file).to_pandas()
 
 level = "All Phase One Products"
 
-file = "./data"+ "/phaseone-tradedata-C-january.parquet"
+file = "./data"+ "/phaseone-tradedata-C-febuary.parquet"
 
 timedf = pq.read_table(file).to_pandas()
 #################################################################################
@@ -360,7 +360,7 @@ def make_time_by_product():
     foobar = make_trade_time(timedf,level_select.value)
     
     start_range = dt.datetime(2017,7,1)
-    end_range = dt.datetime(2021,3,1)
+    end_range = dt.datetime(2021,6,1)
         
     numlines=len(foobar.columns)
     
